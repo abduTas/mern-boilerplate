cb6f63f24655ccdecb0c22cdc5b60a84ed9f320a
Abdulla <abdulla@teamlance.io>
2019-09-12 03:36:27 +0530

    mern boiler init

---
+++ client/src/components/public.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Public extends Component {
+    render() {
+        return (
+            <div className="my-class" >
+                <h1>Publics</h1>
+            </div>
+        );
+    }
+}
